a6e9abb6dc5db48c672b957b5a082c2335bef829
Conrad Hoang <tcmhoang@outlook.com>
2023-09-12 00:42:55 -0400

    integrate svelte into astro

---
+++ src/components/AddToCart.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+
+	export let item: ShopItem;
+	export let addItemToCart: (i:ShopItem) => void;
+
+</script>
+
+<button class="big-link" on:click={() => addItemToCart(item)}>
+	Add To Cart
+</button>
+
