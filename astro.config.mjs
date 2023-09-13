0fc149c6446ab68992ef9b38e821a6e80b75694b
Conrad Hoang <tcmhoang@outlook.com>
2023-09-13 04:24:03 -0400

    ssr form

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import solid from '@astrojs/solid-js';
 
 // https://astro.build/config
 export default defineConfig({
+	output: 'hybrid',
 	site: 'https://example.com',
 	integrations: [svelte(), solid()]
 });
